4153f925b2ed59141dc17bc804bc1bdaab042a52
Andres <andresxmen2020@gmail.com>
2024-09-12 15:59:42 -0500

    commit 2

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const port = process.env.PORT || 3000;
+    const corsOption: CorsOptions = {
+        origin: true,
+        methods: 'POST',
+        credentials: true,
+        preflightContinue: false,
+        optionsSuccessStatus: 204
+    };
+    app.enableCors( corsOption );
     await app.listen( port );
     Logger.log( `Application is running on port ${port}` )
 }
